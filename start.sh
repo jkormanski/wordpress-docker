9abddb90e3398b3e1f40b1cb5a0b7ebc9188f009
jkormanski <jkormanski@gmail.com>
2016-05-19 10:54:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -40,7 +40,7 @@ s/password_here/$WORDPRESS_PASSWORD/
 
 __httpd_perms() {
 adduser user apache
-chown user:apache /var/www/html/wp-config.php  
+chown -R user:apache /var/www  
 chmod u=rwX,g=srX,o=rX -R /var/www
 }
 
